f8ebcbe162ee3c1fcbcc6aaf753b003cec50e426
AntiRain <dailybreak3@gmail.com>
2022-08-24 16:23:15 +0900

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const todoForm = document.querySelector('todoForm');
+const todoList = document.querySelector('todoList');
+const todoInput = document.getElementById('todoInput');
+
+function todoFormSubmit(){
+    console.log('일단 여기까지');
+}
+
+todoForm.addEventListener('submit', todoFormSubmit);
